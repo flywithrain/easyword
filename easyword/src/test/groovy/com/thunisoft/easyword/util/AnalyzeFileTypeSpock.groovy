65493e0952718bf6e62a5cd11a6b19a051ac5c75
wangxiaoyu-2 <wangxiaoyu-2@thunisoft.com>
2019-08-15 18:37:33 +0800

    add：补全spock

---
+++ easyword/src/test/groovy/com/thunisoft/easyword/util/AnalyzeFileTypeSpock.groovy
@@ -0,0 +1,23 @@
+package com.thunisoft.easyword.util
+
+import com.thunisoft.easyword.constant.FileTypeEnum
+import spock.lang.Specification
+
+/**
+ * @author 65751* @date 2019-08-2019/8/15 15:40
+ */
+class AnalyzeFileTypeSpock extends Specification {
+
+    def "analyze the type of the file"() {
+        expect:
+        AnalyzeFileType.getFileType(this.getClass().getClassLoader().getResourceAsStream('\\file\\' + file)) == type
+
+        where:
+        file          | type
+        '1.docx' | -1
+        '1.jpg'  | FileTypeEnum.JPEG.value
+        '1.pdf'  | -1
+        '1.png'  | FileTypeEnum.PNG.value
+    }
+
+}
